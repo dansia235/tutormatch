358f4a473d6585440d2b73a5f85ad9d616b6ee02
Tutoring Admin <admin@tutoring-system.com>
2025-06-12 08:46:18 +0100

    Icône SVG de bâtiment ajoutée comme logo par défaut pour les entreprises.

--- views/admin/companies.php
+++ views/admin/companies.php
@@ -403,13 +403,13 @@ include_once __DIR__ . '/../common/header.php';
                             </div>
                             
                             <div class="d-flex mb-3">
-                                <?php if (!empty($company['logo_path'])): ?>
-                                <img src="<?php echo h($company['logo_path']); ?>" alt="<?php echo h($company['name']); ?>" class="company-logo me-3">
-                                <?php else: ?>
-                                <div class="company-logo-placeholder me-3" style="background-color: <?php echo generateAvatarColor($company['name']); ?>; color: white;">
-                                    <?php echo getInitials($company['name']); ?>
+                                <div class="company-logo-placeholder me-3" style="background-color: <?php echo generateAvatarColor($company['name']); ?>; color: white; display: flex; align-items: center; justify-content: center;">
+                                    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M19 4H5C3.89543 4 3 4.89543 3 6V18C3 19.1046 3.89543 20 5 20H19C20.1046 20 21 19.1046 21 18V6C21 4.89543 20.1046 4 19 4Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M3 8H21" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        <path d="M9 20V8" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
                                 </div>
-                                <?php endif; ?>
                                 
                                 <div>
                                     <h5 class="card-title mb-1"><?php echo h($company['name']); ?></h5>
